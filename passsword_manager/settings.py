ecae046c1b7ece67bfc65dbb3bfb4b2b34391abc
damaneks <56597480+damaneks@users.noreply.github.com>
2022-01-18 09:36:44 +0100

    Update settings.py

--- passsword_manager/settings.py
+++ passsword_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=$42pc#r&e@(9r7t5teg1hp_ak-d&g1s^4!l#aycwkm#n_pq2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
